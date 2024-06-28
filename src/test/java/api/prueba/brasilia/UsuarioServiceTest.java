824365061d14edd98d043843ef59a0f23f3af6cb
Miguel Zambrano <zmiguel96@gmail.com>
2024-06-28 17:21:33 -0500

    Ajuste de prueba unitaria con mockito

--- src/test/java/api/prueba/brasilia/UsuarioServiceTest.java
+++ src/test/java/api/prueba/brasilia/UsuarioServiceTest.java
@@ -1,28 +1,44 @@
 package api.prueba.brasilia;
 
 import api.prueba.brasilia.entity.Usuario;
+import api.prueba.brasilia.repository.UsuarioRepository;
 import api.prueba.brasilia.service.UsuarioService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UsuarioServiceTest {
-    @Autowired
-    private UsuarioService _usuarioService;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void testGuardarUsuario() {
-        // Arrange
         Usuario usuario = new Usuario();
-        usuario.setNombreUsuario("UsuarioTest");
+        usuario.setId(1L);
+        usuario.setNombreUsuario("Usuario test");
 
-        // Act
-        Usuario createdUser = _usuarioService.guardarUsuario(usuario);
+        Mockito.when(usuarioRepository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
 
-        // Assert
-        assertNotNull(createdUser);
-        assertEquals("UsuarioTest", createdUser.getNombreUsuario());
+        Usuario usuarioGuardado = usuarioService.guardarUsuario(usuario);
+
+        Assertions.assertEquals(usuario.getId(), usuarioGuardado.getId());
+        Assertions.assertEquals(usuario.getNombreUsuario(), usuarioGuardado.getNombreUsuario());
     }
 }
